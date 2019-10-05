3d5baca85b318cef62c7181d631dcb8227b6b402
Matthew Waler <mattwaler@Matthews-MBP.attlocal.net>
2019-10-05 11:32:38 -0400

    Adding ESLINT!

--- scripts/imports/helpers.js
+++ scripts/imports/helpers.js
@@ -1,6 +1,6 @@
-const isDev = process.env.NODE_ENV != 'production'
+const isDev = process.env.NODE_ENV !== 'production'
 
-export function run(el, fn) {
+export function run (el, fn) {
   const matches = Array.from(document.querySelectorAll(el))
   if (matches.length > 0) {
     return matches.forEach(match => {
@@ -11,6 +11,6 @@ export function run(el, fn) {
   }
 }
 
-export function click(el, fn) {
+export function click (el, fn) {
   el.addEventListener('click', fn)
 }
